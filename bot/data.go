021e074e8a72300cbafad4793746b1ff67754fa6
Dominic Pettifer <sironfoot@gmail.com>
2016-03-19 10:46:38 +0000

    added: function to Save Tweets to json file

--- bot/data.go
+++ bot/data.go
@@ -29,3 +29,17 @@ func LoadTweets(dataFile string) ([]Tweet, error) {
 
 	return tweets, err
 }
+
+// SaveTweets saved an array of Tweet structs to a json data file
+func SaveTweets(tweets []Tweet, dataFile string) error {
+	tweetData, err := json.MarshalIndent(&tweets, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(dataFile, tweetData, os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
--- bot/data_test.go
+++ bot/data_test.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"testing"
 	"unicode/utf8"
 )
 
-func TestLoadingData(t *testing.T) {
+func TestLoadTweets(t *testing.T) {
 	tweets, err := LoadTweets("tweets.json")
 	if err != nil {
 		t.Fatalf("Failed to load tweets: %s", err)
@@ -15,8 +15,46 @@ func TestLoadingData(t *testing.T) {
 	if len(tweets) == 0 {
 		t.Error("Should be at least 1 tweet")
 	}
+}
 
-	fmt.Printf("%d tweets were returned\n", len(tweets))
+func TestSaveTweets(t *testing.T) {
+	tweetFile := "tweets_test.json"
+
+	tweets := []Tweet{
+		Tweet{
+			Text:     "Tweet 1",
+			IsPosted: false,
+		},
+		Tweet{
+			Text:     "Tweet 2",
+			IsPosted: true,
+		},
+	}
+
+	err := SaveTweets(tweets, tweetFile)
+	if err != nil {
+		t.Fatalf("Failed to save tweets: %s", err)
+	}
+
+	defer os.Remove(tweetFile)
+
+	savedTweets, err := LoadTweets(tweetFile)
+	if err != nil {
+		t.Fatalf("Failed to load tweets: %s", err)
+	}
+
+	if len(savedTweets) != len(tweets) {
+		t.Fatalf("Number of tweets (%d) not the same as number saved (%d)", len(tweets), len(savedTweets))
+	}
+
+	for i := range tweets {
+		tweet := tweets[i]
+		savedTweet := savedTweets[i]
+
+		if tweet.Text != savedTweet.Text && tweet.IsPosted != savedTweet.IsPosted {
+			t.Errorf("tweet at index: %d doesn't match saved tweet", i)
+		}
+	}
 }
 
 func TestTweetMaxLength(t *testing.T) {
