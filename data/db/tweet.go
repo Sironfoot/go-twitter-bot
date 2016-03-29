53b77118d6a6de5f0566c904dd769e13b21dbb8f
Dominic Pettifer <sironfoot@gmail.com>
2016-03-29 20:04:58 +0100

    added: functions for saving and deleting Tweets.

--- data/db/tweet.go
+++ data/db/tweet.go
@@ -1,6 +1,9 @@
 package db
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Tweet maps to tweets table
 type Tweet struct {
@@ -15,4 +18,49 @@ type Tweet struct {
 // ID returns read-only Primary Key ID of Tweet
 func (tweet *Tweet) ID() string {
 	return tweet.id
+}
+
+// IsTransient determines if Tweet record has been saved to the database,
+// true means Tweet struct has NOT been saved, false means it has.
+func (tweet *Tweet) IsTransient() bool {
+	return len(tweet.id) == 0
+}
+
+// Save saves the Tweet struct to the database.
+func (tweet *Tweet) Save() error {
+	if tweet.IsTransient() {
+		if tweet.Account == nil {
+			return fmt.Errorf("Parent TwitterAccount entity must be set")
+		}
+
+		sql := "INSERT INTO tweets(twitter_acount_id, tweet, post_on, is_posted, date_created) " +
+			"VALUES($1, $2, $3, $4, $5) RETURNING id"
+
+		statement, err := db.Prepare(sql)
+		if err != nil {
+			return err
+		}
+		defer statement.Close()
+
+		err = statement.
+			QueryRow(tweet.Account.id, tweet.Tweet, tweet.PostOn, tweet.IsPosted, tweet.DateCreated).
+			Scan(&tweet.id)
+		if err != nil {
+			return err
+		}
+	} else {
+		_, err := db.Exec("UPDATE tweets SET tweet = $2, post_on = $3, is_posted = $4, date_created = $5 WHERE id = $1",
+			tweet.id, tweet.Tweet, tweet.PostOn, tweet.IsPosted, tweet.DateCreated)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Delete deletes the Tweet from the database
+func (tweet *Tweet) Delete() error {
+	_, err := db.Exec("DELETE FROM tweets WHERE id = $1", tweet.id)
+	return err
 }
